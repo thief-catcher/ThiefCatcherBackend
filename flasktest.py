875316bfac95fe2336303ca3a61bcc1c6c587e3f
jBosak98 <jbosak98@gmail.com>
2019-05-17 20:15:42 +0200

    takePhoto()

--- flasktest.py
+++ flasktest.py
@@ -14,19 +14,22 @@ SIZE = (640, 480)
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-@app.route('/api/capture')
-def capture():
-    FILENAME = CAPTURES_DIR + datetime.datetime.now().isoformat() + ".jpg"
-    cv2.imwrite(FILENAME, camera.read()[1])
-    return send_file(FILENAME, mimetype='image/jpg')
+
+
+
+@app.route('/api/images/capture')
+def takePhoto():
+    cv2.imwrite(CAPTURES_DIR + datetime.datetime.now().isoformat() + ".jpg", camera.read()[1])
+    return """"""
+
 
 @app.route('/api/images')
 def images():
     onlyfiles = [{"name": f} for f in listdir(CAPTURES_DIR) if isfile(join(CAPTURES_DIR, f))]
     return jsonify(onlyfiles)
 
+
 @app.route('/api/images/<img>')
 def showimage(img):
     FILENAME = CAPTURES_DIR + img
     return send_file(FILENAME, mimetype='image/jpg')
-

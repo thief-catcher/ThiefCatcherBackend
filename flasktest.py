c57625d2b78f58c617c53a389e0099a015e791d6
Szymekc <szymekc98@gmail.com>
2019-05-19 21:56:33 +0200

    Work in progress buzzer and motionsensor

--- flasktest.py
+++ flasktest.py
@@ -7,12 +7,15 @@ import cv2
 from flask import Flask
 from flask import jsonify
 from flask import send_file
+from gpiozero import MotionSensor, Buzzer
 
 DEVICE = '/dev/video0'
 CAPTURES_DIR = os.getcwd() + '/captures/'
 SIZE = (640, 480)
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
+sensor = MotionSensor(1) #GPIO slot for sensor =1
+buzzer = Buzzer(2)
 
 @app.route('/api/capture')
 def capture():
@@ -30,3 +33,13 @@ def showimage(img):
     FILENAME = CAPTURES_DIR + img
     return send_file(FILENAME, mimetype='image/jpg')
 
+@app.route('/api/mute')
+def mute_buzzer():
+    buzzer.off()
+
+def motion_capture():
+    capture()
+    buzzer.on()
+
+
+sensor.when_motion = motion_capture

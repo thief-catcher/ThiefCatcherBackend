fd1387b4001d6196f8787eb30ecdee46371dfce7
jBosak98 <jbosak98@gmail.com>
2019-05-28 09:15:22 +0200

    toggle_buzzer

--- flasktest.py
+++ flasktest.py
@@ -20,9 +20,14 @@ CAPTURES_DIR = os.getcwd() + '/captures/'
 SIZE = (640, 480)
 app = Flask(__name__)
 cam = Camera()
-sensor = MotionSensor(24) #GPIO slot for sensor =1
+sensor = MotionSensor(24)  # GPIO slot for sensor =1
 buzzer = Buzzer(23)
 door = Button(18)
+
+
+buzzer.on()
+time.sleep(1)
+buzzer.off()
 
 
 @app.route('/api/capture')
@@ -30,10 +35,12 @@ def capture():
     FILENAME = CAPTURES_DIR + datetime.datetime.now().isoformat() + ".jpg"
     cv2.imwrite(FILENAME, cam.camera.read()[1])
     return send_file(FILENAME, mimetype='image/jpg')
-    
+
+
 def capture_nohttp():
     FILENAME = CAPTURES_DIR + datetime.datetime.now().isoformat() + ".jpg"
     cv2.imwrite(FILENAME, cam.camera.read()[1])
+
 
 @app.route('/api/images')
 def images():
@@ -46,6 +53,7 @@ def showimage(img):
     FILENAME = CAPTURES_DIR + img
     return send_file(FILENAME, mimetype='image/jpg')
 
+
 @app.route('/api/live')
 def livestream():
     return Response(gen(cam), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -57,39 +65,47 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/api/mute')
-def mute_buzzer():
-    buzzer.off()
-    print(door.value)
+
+@app.route('/api/toggle')
+def toggle_buzzer():
+    if buzzer.is_active:
+        buzzer.off()
+    else:
+        buzzer.on()
+
 
 
 def door_unlock():
     print("Door unlocked, starting stream")
-    gen(cam)
+    # gen(cam)
+
 
 def async(f):
     def wrapper(*args, **kwargs):
         thr = Thread(target=f, args=args, kwargs=kwargs)
         thr.start()
+
     return wrapper
-    
+
+
 @async
 def motion_capture():
     with app.app_context():
         capture_nohttp()
         print("motion detected")
         buzzer.on()
+        time.sleep(0.5)
+        buzzer.off()
+
 
 @async
 def fsensor():
     with app.app_context():
         sensor.when_motion = motion_capture
-        sensor.when_no_motion = mute_buzzer
         door.when_released = door_unlock
+
 
 fsensor()
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
-
